e29358e4d418a9a6ef129b1f1e2d374b5bb3c210
加帆 <jiafan.zhang@qq.com>
2015-11-23 19:00:11 +0800

    定义命令参数

--- ngx_http_single_upload_module.c
+++ ngx_http_single_upload_module.c
@@ -4,8 +4,7 @@
 #include <ngx_http.h>
 
 typedef struct {
-	ngx_uint_t status;
-	ngx_str_t msg;
+	ngx_str_t file_name;
 }ngx_http_single_upload_loc_conf_t;
 
 static char* ngx_http_cname(ngx_conf_t* cf,ngx_command_t* cmd,void* conf);
@@ -20,7 +19,7 @@ static ngx_command_t ngx_http_single_upload_commands[]={
 		NGX_HTTP_LOC_CONF|NGX_CONF_TAKE1,
 		ngx_http_cname,
 		NGX_HTTP_LOC_CONF_OFFSET,
-		offsetof(ngx_http_print_loc_conf_t,output_words),
+		offsetof(ngx_http_print_loc_conf_t,file_name),
 		NULL
 	},
 	ngx_null_command
